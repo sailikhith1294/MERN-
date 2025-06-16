628962798da20f295d4315eddf0516f72a62194f
Sai Likhith <likhithgolagani1294@gmail.com>
2025-06-16 13:32:31 +0530

    day1

---
+++ day1/app.js
@@ -0,0 +1,12 @@
+// window.console.log("hey, hii....");
+
+// let age=21;
+// const name="John";
+// var code="JS101";
+
+// console.log(age, name, code);
+
+let age=22;
+console.log(age);
+age=24;
+console.log(age);
